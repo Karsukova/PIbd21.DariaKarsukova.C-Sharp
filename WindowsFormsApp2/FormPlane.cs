28405440b04a22aa714901d45cf20120c04130cc
Daria Karsukova <karsukova2011@mail.ru>
2018-12-21 20:38:42 +0400

    Лаба №6

--- WindowsFormsApp2/FormPlane.cs
+++ WindowsFormsApp2/FormPlane.cs
@@ -11,7 +11,7 @@ namespace WindowsFormsPlane
     public partial class FormPlane : Form
     {
 
-        MultiLevelParking parking;
+       MultiLevelParking parking;
         /// <summary>
         /// Форма для добавления
         /// </summary>
@@ -44,6 +44,39 @@ namespace WindowsFormsPlane
                 Graphics gr = Graphics.FromImage(bmp);
                 parking[listBoxLevels.SelectedIndex].Draw(gr);
                 pictureFighterPark.Image = bmp;
+            }
+        }
+        /// <summary>
+        /// Обработка нажатия кнопки "Забрать"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonTakeCar_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                if (maskedTextBox.Text != "")
+                {
+                    var plane = parking[listBoxLevels.SelectedIndex] -
+                   Convert.ToInt32(maskedTextBox.Text);
+                    if (plane != null)
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTakePlane.Width,
+                       pictureBoxTakePlane.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        plane.SetPosition(5, 5, pictureBoxTakePlane.Width,
+                       pictureBoxTakePlane.Height);
+                        plane.DrawFighter(gr);
+                        pictureBoxTakePlane.Image = bmp;
+                    }
+                    else
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTakePlane.Width,
+                       pictureBoxTakePlane.Height);
+                        pictureBoxTakePlane.Image = bmp;
+                    }
+                    Draw();
+                }
             }
         }
         /// <summary>
@@ -94,6 +127,14 @@ namespace WindowsFormsPlane
             form.AddEvent(AddPlane);
             form.Show();
         }
+
+       
+
+        private void listBoxLevels_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
         private void buttonTakePlane_Click(object sender, EventArgs e)
         {
             if (listBoxLevels.SelectedIndex > -1)
@@ -120,6 +161,41 @@ namespace WindowsFormsPlane
                     }
                     Draw();
                 }
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (parking.SaveData(saveFileDialog.FileName))
+                {
+                    MessageBox.Show("Сохранение прошло успешно", "Результат",
+                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Не сохранилось", "Результат", MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (parking.LoadData(openFileDialog.FileName))
+                {
+                    MessageBox.Show("Загрузили", "Результат", MessageBoxButtons.OK,
+                   MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Не загрузили", "Результат", MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+                }
+                Draw();
             }
         }
     }
